04a4de83a56f79750bf669de1be849e2ecb71d1b
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-11-10 17:23:17 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,17 @@
 import json
 
-def test_diff_outputs_json_with_expected():
-    """
-    Test that result is the same
-    """
-    failed = False
+def compare_json(file1_path, file2_path):
+    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
+        json1 = json.load(file1)
+        json2 = json.load(file2)
 
-    try:
-        # Save the output of both commands to files
-        run_shell_test(f'cat out2.json | ./jq -e . > 1.test')
-        run_shell_test('cat output.json | ./jq -e . > 2.test')
+        if json1 == json2:
+            print("JSON files are identical.")
+        else:
+            print("JSON files are different.")
 
-        # Compare the contents of the two test files
-        diff_result = run_shell_test('diff 1.test 2.test', capture_output=True)
-        if diff_result.returncode != 0:
-            print("Difference in test files:")
-            print(diff_result.stdout.decode('utf-8'))
-            failed = True
+if __name__ == "__main__":
+    file1_path = "output.json"
+    file2_path = "out2.json"
+    compare_json(file1_path, file2_path)
 
-            # Save the differences to diff.json
-            differences = {
-                "diff_output": diff_result.stdout.decode('utf-8')
-            }
-            with open('diff.json', 'w') as diff_file:
-                json.dump(differences, diff_file)
-
-    except CalledProcessError as e:
-        failed = True
-        result = e.output
-        code = e.returncode
-
-    if failed:
-        pytest.fail(f"Return code: {code} Script output: {result}")
